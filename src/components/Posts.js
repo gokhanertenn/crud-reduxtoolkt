2c49567f4d903ff2eaa1f422c6d982d721198fd5
gokhan <gokhanerten31@icloud.com>
2023-01-20 14:15:36 +0200

    finaly

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,11 +1,15 @@
 import React, { useState } from 'react'
 import { useDispatch,useSelector } from 'react-redux';
-import { addPost } from './redux/postsSlice';
+import { addPost,deletePost,updatePost } from './redux/postsSlice';
 
 function Posts() {
    
     const [title,setTitle] = useState("");
     const [desc,setDesc] = useState("");
+    const [upTitle,setUpTitle] = useState("");
+    const [upDesc,setUpDesc] = useState("");
+    const [edit,setEdit] = useState(false);
+    const [id,setId] = useState(null);
     const posts = useSelector((state) => state.post.items)
     
     const dispatch = useDispatch();
@@ -24,18 +28,55 @@ function Posts() {
         placeholder='Enter Posts Descriptions'
         onChange={(e) => setDesc(e.target.value)}
         ></input>
-        <button onClick={() => dispatch(addPost({id:posts.length + 1,title,desc}) )}
+        <button onClick={
+          () =>{ dispatch(addPost({id:posts.length + 1,title,desc}) )
+        setTitle("");
+        setDesc("");
+        
+        }}
+        
         >ADD</button>
       </div>
 
        <div className='posts'>
        
-        {posts.length > 0 ?    posts.map(posts => <div className='posts'>
+        {posts.length > 0 ?    posts.map(posts => <div key={posts.id} className='posts'>
                <h2>{posts.title}</h2>
                <p>{posts.desc}</p>
-               <button>Edit</button>
-               <button>Delete</button>
-               <button>Update</button>
+               <button onClick={() =>{
+                 setEdit(true)
+                 setId(posts.id)
+               }
+               
+                
+                }>Edit</button>
+               <button onClick={() => dispatch(deletePost({id:posts.id}))}>Delete</button>
+              
+              {edit &&  id == posts.id &&   
+                 
+              <>
+              
+                <input 
+                type="text"
+                placeholder='Edit'
+                onChange={(e) => setUpTitle(e.target.value)}
+                ></input>
+                <input 
+                type="text"
+                placeholder='Edit'
+                onChange={(e) => setUpDesc(e.target.value)}
+                ></input>
+                <button onClick={() => {dispatch(updatePost({id:posts.id,title:upTitle,desc:upDesc})
+               
+                )
+                
+                setEdit(false)
+                }}>Update</button>
+              
+              </>
+              
+              
+              }
 
 
 
--- src/components/redux/postsSlice.js
+++ src/components/redux/postsSlice.js
@@ -14,11 +14,23 @@ const postsSlice = createSlice({
         addPost: function(state,action){
           state.items.push(action.payload)
           
+        },
+        deletePost: function(state,action) {
+            state.items = state.items.filter(item => item.id != action.payload.id)
+        },
+        updatePost: function(state,action) {
+            state.items.map(item => {
+
+                if(item.id == action.payload.id) {
+                    item.title = action.payload.title
+                    item.desc = action.payload.desc
+                }
+            })
         }
 
     }
 })
 
-export const { addPost} = postsSlice.actions
+export const { addPost,deletePost,updatePost} = postsSlice.actions
 
 export default postsSlice.reducer
